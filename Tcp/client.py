bec2ae6a5b0ffc80d88bef063dc70ceb8f23d6b0
msean <674645611@qq.com>
2017-04-12 15:00:17 +0800

    new file:   Tcp/client.py 	new file:   Tcp/server.py

---
+++ Tcp/client.py
@@ -0,0 +1,25 @@
+from twisted.internet import reactor,protocol
+
+
+class EchoClient(protocol.Protocol):
+    def connectionMade(self):
+        self.transport.write("Hello World!".encode())
+
+    def dataReceived(self, data):
+        print("Server said:",data)
+        self.transport.loseConnection()
+
+class EchoFactory(protocol.ClientFactory):
+    def buildProtocol(self,addr):
+        return EchoClient()
+
+    def clientConnectFailed(self,connector,reason):
+        print("conection failed")
+        reactor.stop()
+
+    def connectionLost(self, reason):
+        print("conection lost")
+        reactor.stop()
+
+reactor.connectTCP("localhost",8001,EchoFactory())
+reactor.run()
